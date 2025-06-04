cb108beea15b8bca1bf03deeec6da54aae7f7564
kishanghosh090 <kishanghosh090@gmail.com>
2025-06-05 00:57:24 +0530

    complete objects in ts

--- 01_basics/array.ts
+++ 01_basics/array.ts
@@ -1,25 +1,27 @@
 const superHeros: string[] = [];
-const hersoPower: Array<number> = [];
+const heroPower: Array<number> = [];
 
 type User = {
   name: string;
   isActive: boolean;
 };
 
-const allUsers: Array<User> = [];
+// const allUsers: Array<User> = [];
+const allUsers: User[] = [];
 
 const MLModels: number[][] = [
-  [233,45,23],
-  [344,34,45]
-]
+  [233, 45, 23],
+  [344, 34, 45],
+];
 superHeros.push("Iron");
 superHeros.push("Spider");
 superHeros.push("Thor");
+// superHeros.push(12)
 
-hersoPower.push(100);
+heroPower.push(100);
 
 allUsers.push({ name: "Iron", isActive: true });
 allUsers.push({ name: "Spider", isActive: false });
+// allUsers.push(12) // error because it is not an object we are pushing and we declared it as an object in the type : User
 
-
-export {}
+export {};
