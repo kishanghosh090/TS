ab3feb889154675c801925235f59eb4a5e2fc177
kishanghosh090 <kishanghosh090@gmail.com>
2025-06-05 01:35:59 +0530

    complete union in ts

--- 01_basics/union.ts
+++ 01_basics/union.ts
@@ -1,41 +1,44 @@
-let score: number | string = 33
+let score: number | string = 33; // union(|)
 
-score = 44
-score = "55"
-
+score = 44;
+score = "55";
 
 type User = {
-  name: string,
-  id: number
-}
+  name: string;
+  id: number;
+};
 type Admin = {
-  userName: string,
-  id: number
-}
+  userName: string;
+  id: number;
+};
 let hitesh: User | Admin = {
-  name: "kishan",id:232
+  name: "kishan",
+  id: 232,
+};
+
+hitesh = { userName: "hc", id: 234 };
+
+function getDBId(id: number | string) {
+  // making some API call
+  console.log(`DB id is${id}`);
 }
 
-hitesh = {userName: "hc", id:234}
+getId(234);
+getId("324");
 
-// function getId(id:number | string) {
-//   // making some API call
-//   console.log(`DB id is${id}`)
-// }
-
-getId(234)
-getId("324")
-
-function getId(id:number | string) {
-  if(typeof id == "string"){
-    id.toUpperCase()
+function getId(id: number | string) {
+  if (typeof id == "string") {
+    id.toUpperCase();
+    return;
   }
+  id + 2;
 }
 
-const data: number[] = [1,2,3,"4"]
-const data2: (number | string)[] = ["1","34","4"]
+// const data: number[] | string[] = [1,2,3,"4"] // error because it is not an array of numbers and strings
 
-let pi:3.14 = 3.14
+const data2: (number | string)[] = ["1", "34", "4"];
 
+let pi: 3.14 = 3.14;
 
-let seatAllotment: "aisle" | "middle" | "window"
+let seatAllotment: "aisle" | "middle" | "window";
+seatAllotment = "aisle";
