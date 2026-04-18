5e560f9af5c6c4aca1cfcf0f61e80232deebcd17
kishanghosh090 <kishanghosh090@gmail.com>
2026-04-18 19:20:48 +0530

    rivison type anotation and type inference

---
+++ rivison/dist/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function getTea(name) {
+    return `do you want ${name}?`;
+}
+console.log(getTea("alychi"));
+//# sourceMappingURL=index.js.map
---
+++ rivison/src/typeanotationandinference/typesInTs.ts
@@ -0,0 +1,11 @@
+let drink = "chai";
+
+let cups = Math.random() > 0.5 ? 10 : "5";
+
+// cups = undefined; // Type 'undefined' is not assignable to type 'string | number'.ts(2322)
+
+let chaiFlavour: string = "alychi chai";
+chaiFlavour = "lemon chai";
+
+let chaiOrder: number = 8;
+let isAvailable: boolean;
